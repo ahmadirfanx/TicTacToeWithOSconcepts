5d81976d5d6f704d1d31b0cd6fd98fb8cf836e25
Ahmad Irfan <ahmadirfanx@gmail.com>
2019-05-26 19:50:14 +0000

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -11,11 +11,12 @@ bool PlaceMove();
 bool ValidateMove();
 void ChangeTurns();
 bool CheckWinningCondition();
+bool CheckIfFull();
 
 pthread_mutex_t lock;  //mutex lock
 char array[9];
 char CurrentTurn='X';
-char Winner='Z';
+char Winner=0;
 
 int main(){ //--------------------------------------main
 pthread_t main_thread; //parent thread
@@ -24,13 +25,16 @@ pthread_join(main_thread, NULL);
 bool terminate=false;
 cout << "Ola!" << endl;
 while (terminate==false){
+	terminate= PlaceMove();
+	if (!terminate){
+	terminate= CheckIfFull();
+	}
+	cout << "terminate: " << terminate << endl;
 
 	pthread_t th2; //thread for print function
 	pthread_create(&th2, NULL, printBoard, NULL); 
 	pthread_join(th2, NULL);
-
-	terminate= PlaceMove();
-
+	
 	ChangeTurns();
 }
 return 0;
@@ -43,13 +47,15 @@ array[i]=' ';
 }
 
 void *printBoard(void *args){
+int count =0;
 cout << "\t *************************" << endl;
 for (int j=0; j<3; j++){
 	for (int k=0; k<3;k++){
 		if (k==1){
 		cout << "\t |";
 			for (int p=0; p<3;p++){
-				cout <<"   " << array[p] << "   |";
+				cout <<"   " << array[count] << "   |";
+				count++;
 			}
 		cout << endl;
 		}
@@ -71,11 +77,16 @@ int choice;
 cin >> choice;
 if (choice >=0 && choice<=9 && ValidateMove(choice)){
 	array[choice]=CurrentTurn;
-cout << "Array updated, index: " << choice << "array: " <<array[choice] << endl;
+	cout << "Array updated, index: " << choice << " array: " <<array[choice] << endl;
+	if (CheckWinningCondition()){
+		return true;
 	}
-if (CheckWinningCondition())
-return true;
-else return false;
+	else return false;
+	}
+else {
+	cout << "invalid number entered. Enter a Valid number..." << endl;
+	PlaceMove();
+	}
 }
 
 bool ValidateMove(int x){	
@@ -88,21 +99,47 @@ else return false;
 bool CheckWinningCondition(){
 
 if (array[0]==array[1] && array[1]==array[2] && (array[2]=='X' || array[2]=='O')) Winner=array[2];
-else if (array[0]==array[3] && array[3]==array[6] && (array[6]=='X' || array[2]=='O')) Winner=array[6];
-else if (array[2]==array[5] && array[5]==array[8] && (array[8]=='X' || array[2]=='O')) Winner=array[8];
-else if (array[6]==array[7] && array[7]==array[8] && (array[8]=='X' || array[2]=='O')) Winner=array[8];
-else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')) Winner=array[7];
-else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')) Winner=array[7];
-else if (array[3]==array[4] && array[4]==array[5] && (array[5]=='X' || array[2]=='O')) Winner=array[5];
-else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')) Winner=array[7];
-else if (array[0]==array[4] && array[4]==array[8] && (array[8]=='X' || array[2]=='O')) Winner=array[8];
-else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')) Winner=array[7];
-else if (array[2]==array[4] && array[4]==array[6] && (array[6]=='X' || array[2]=='O')) Winner=array[6];
+else if (array[0]==array[3] && array[3]==array[6] && (array[6]=='X' || array[2]=='O')){
+ Winner=array[6];}
+else if (array[2]==array[5] && array[5]==array[8] && (array[8]=='X' || array[2]=='O')){ 
+Winner=array[8]; }
+else if (array[6]==array[7] && array[7]==array[8] && (array[8]=='X' || array[2]=='O')){
+ Winner=array[8];}
+else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')){
+ Winner=array[7];}
+else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')){
+ Winner=array[7];}
+else if (array[3]==array[4] && array[4]==array[5] && (array[5]=='X' || array[2]=='O')){
+ Winner=array[5];}
+else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')){
+ Winner=array[7];}
+else if (array[0]==array[4] && array[4]==array[8] && (array[8]=='X' || array[2]=='O')){
+ Winner=array[8];}
+else if (array[1]==array[4] && array[4]==array[7] && (array[7]=='X' || array[2]=='O')){
+ Winner=array[7];}
+else if (array[2]==array[4] && array[4]==array[6] && (array[6]=='X' || array[2]=='O')){
+ Winner=array[6];}
 
+
+if (Winner=='X' || Winner=='O'){
 cout << "Winner found: "<< Winner << endl;
-if (Winner=='X' || Winner=='O')
 return true;
-else false;
+}
+
+else return false;
+}
+
+bool CheckIfFull(){
+	int count=0;
+	for (int i=0; i<9; i++){
+	if (array[i]=='X' || array[i]=='O'){
+		count++;
+		}
+	}
+	if (count==9){
+return true;
+}
+else return false; 
 }
 
 void ChangeTurns(){
